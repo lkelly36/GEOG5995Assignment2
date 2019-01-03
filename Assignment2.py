3d664b9345a4385c904dcfd6580de0fc3e703b55
Louise Kelly <louuise.kelly@hotmail.com>
2019-01-03 16:02:01 +0000

    finished commit

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,6 @@
 """
+===============================================================================
+
 Assignment 2
 GEOG5995M Programming for Social Scientists
 University of Leeds student ID number: 201282995
@@ -9,10 +11,14 @@ some descriptive statistics and analyse the data using regression models.
 The data set used for this assignment was the Smoking, Drinking and Drug Use 
 Among Young People (2016), which can be obtained from 
 https://beta.ukdataservice.ac.uk/datacatalogue/studies/study?id=8320
+
+===============================================================================
+
 """
 
 """
 Import libraries and data set using Pandas to convert .tab file
+https://www.pandas.pydata.org
 """
 # Import required libraries
 
@@ -20,7 +26,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import scipy as sp
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 # Read in the data set
 
@@ -56,7 +63,7 @@ def CleanData(df1):
     df1.devrpsy.replace(nan_values, np.nan, inplace=True)
     df1.devropi.replace(nan_values, np.nan, inplace=True)
     df1.devrcla.replace(nan_values, np.nan, inplace=True)
-    df1.devrrps.replace(nan_values, np.nan, inplace=True)
+    df1.devrps.replace(nan_values, np.nan, inplace=True)
     df1.ddgany.replace(nan_values, np.nan, inplace=True)
     
 def CleanWell(df1):
@@ -71,78 +78,29 @@ CleanWell(df1)
 # Change NaNs to average mean
 df1 = df1.fillna(df1.mean())
 df1.head()
-
 # Check datatype
 df1.info()
-
 # Change floats to int
 df1 = df1.astype(int)
-
-# Create functions for binary variables
-
-def CleanBin(df1):
-   
-    # Replace sex variables
-    df1.sex.replace(1.0, 'male', inplace=True)
-    df1.sex.replace(2.0, 'female', inplace=True)
-    
-    # Replace ever tried drug variables
-    df1.dgtdcan.replace(1.0, 'yes', inplace=True)
-    df1.dgtdcan.replace(2.0, 'no', inplace=True)
-    df1.dgtdamp.replace(1.0, 'yes', inplace=True)
-    df1.dgtdamp.replace(2.0, 'no', inplace=True)
-    df1.dgtdlsd.replace(1.0, 'yes', inplace=True)
-    df1.dgtdlsd.replace(2.0, 'no', inplace=True)
-    df1.dgtdecs.replace(1.0, 'yes', inplace=True)
-    df1.dgtdecs.replace(2.0, 'no', inplace=True)
-    df1.dgtdcok.replace(1.0, 'yes', inplace=True)
-    df1.dgtdcok.replace(2.0, 'no', inplace=True)
-    df1.dgtdket.replace(1.0, 'yes', inplace=True)
-    df1.dgtdket.replace(2.0, 'no', inplace=True)
-    df1.dgtdnox.replace(1.0, 'yes', inplace=True)
-    df1.dgtdnox.replace(2.0, 'no', inplace=True)
-    df1.dgtdleg.replace(1.0, 'yes', inplace=True)
-    df1.dgtdleg.replace(2.0, 'no', inplace=True)
-    
-    # Replace ever tried drug group variables
-    df1.devrstm.replace(1.0, 'yes', inplace=True)
-    df1.devrstm.replace(2.0, 'no', inplace=True)
-    df1.devrpsy.replace(1.0, 'yes', inplace=True)
-    df1.devrpsy.replace(2.0, 'no', inplace=True)
-    df1.devropi.replace(1.0, 'yes', inplace=True)
-    df1.devropi.replace(2.0, 'no', inplace=True)
-    df1.devrcla.replace(1.0, 'yes', inplace=True)
-    df1.devrcla.replace(2.0, 'no', inplace=True)
-    df1.devrps.replace(1.0, 'yes', inplace=True)
-    df1.devrps.replace(2.0, 'no', inplace=True)
-    
-    # Replace ever tried any drug
-    df1.ddgany.replace(1.0, 'yes', inplace=True)
-    df1.ddgany.replace(2.0, 'no', inplace=True)
-    
-    # Replace wellbeing category
-    df1.ddwbcat.replace(1.0, 'low wellbeing', inplace=True)
-    df1.ddwbcat.replace(2.0, 'not low wellbeing', inplace=True)
-
-# Run function for binary variables
-CleanBin(df1)
-
 # Check data
 df1.head()
 
-"""
-Descriptive Statistics and Data Visualisation using Numpy and Seaborn
-"""
+# Define binary sex  and drug variables
 
-# Create plot to show distribution of wellbeing score
-#sns.distplot(df1.ddwbscore)
-#plt.title('Histogram of Wellbeing Scores')
-#plt.xlabel('Wellbeing Scores')
-#plt.ylabel('Estimated Density')
-#plt.xticks((0,2,4,6,8,10,12,14,16,18,20))
-#plt.xlim([0,20])
-#plt.savefig('wellbeing_hist.jpg')
-#plt.figure()
+def CleanBin(df1):
+    # Replace sex variables
+    df1.sex.replace(1.0, 'male', inplace=True)
+    df1.sex.replace(2.0, 'female', inplace=True)
+    # Replace ever tried any drug
+    df1.ddgany.replace(1.0, 'yes', inplace=True)
+    df1.ddgany.replace(2.0, 'no', inplace=True)
+
+CleanBin(df1)
+
+"""
+Descriptive Statistics and Data Visualisation using Seaborn
+https://www.seaborn.pydata.org
+"""
 
 # Calculate descriptive statistics
 
@@ -151,25 +109,74 @@ wbvar = np.var(df1.ddwbscore) # variance
 print(wbmean)
 print(wbvar)
 
-# Set parameters for visualising central tendency of wellbeing scores
-xk,yk = ax.get_lines()[0].get_data()
-mm = np.mean(df1.ddwbscore)
-md = np.median(df1.ddwbscore)
-mo = xk[np.argmax(yk)]
+# Create matrix with variables of interest and assign to array
+#X = [df1.ddwbscore, df1.ddwbcat, df1.dgtdcan, df1.dgtdamp, df1.dgtdlsd, 
+ #    df1.dgtdecs, df1.dgtdcok, df1.dgtdket, df1.dgtdnox, df1.dgtdleg, 
+  #   df1.devrstm, df1.devrpsy, df1.devropi, df1.devrcla, df1.devrps, df1.ddgany]
+# Get sample means
+#xbar = np.mean(X,1)
+#print(xbar)
 
-# Plot central tendency of wellbeing scores - including mean, median and KDE estimated mode
-plt.figure(figsize=(5,2))
-plt.plot(xk,yk,'-k')
-xx = np.ones(2)
-yy = np.array([0, 0.8])
-plt.plot(mm*xx,yy,'--b',label='Mean')
-plt.plot(md*xx,yy,'-.r',label='Median')
-plt.plot(mo*xx,yy,':m',label='KDE-estimated Mode')
-plt.xlabel('Wellbeing Score')
-plt.ylabel('Estimated Density')
-plt.xlim([0,20])
-plt.legend()
-plt.tight_layout()
-plt.savefig('wellbeing_cent_tend.jpg')
-plt.show()
-    
+# Bar chart of drug use on wellbeing
+sns.barplot(x='ddgany', y='ddwbscore', hue='sex', data=df1)
+#Set labels, save and show plot
+plt.title('Relationship between drug use and wellbeing')
+plt.xlabel('Ever used any drugs')
+plt.ylabel('Wellbeing Scores')
+plt.savefig('../bar_wb_drug.jpg',format='jpg')
+plt.figure()
+
+"""
+Linear regression showing drug use and gender as predictors of wellbeing.
+Using seaborn for visualisation and statsmodels for regression:
+https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.OLS.html
+"""
+
+# Print regression model
+model = ols("ddwbscore ~ ddgany + sex", df1).fit()
+print(model.summary())
+
+"""
+Linear regression showing use of different drugs as predictors of wellbeing.
+"""
+
+# As more variables, quicker to create X variable
+X = [df1.dgtdcan, df1.dgtdamp, df1.dgtdlsd, df1.dgtdecs, df1.dgtdcok, 
+     df1.dgtdket, df1.dgtdnox, df1.dgtdleg]
+X = np.array(X)
+X = X.T
+X = sm.add_constant(X) # Include constant in regression
+# Create response variable - wellbeing scores
+y = df1.ddwbscore
+
+# Run linear regression model and print summary
+linear_model=sm.OLS(y,X)
+result_lin=linear_model.fit()
+print(result_lin.summary2())
+
+# Remove insignificant variables from model
+X = [df1.dgtdcan, df1.dgtdamp, df1.dgtdlsd, df1.dgtdecs, df1.dgtdcok, 
+     df1.dgtdket, df1.dgtdleg]
+X = np.array(X)
+X = X.T
+X = sm.add_constant(X) # Include constant
+
+# Run second linear regression model without insignificant variables
+linear_model2=sm.OLS(y,X)
+result_lin2=linear_model2.fit()
+print(result_lin2.summary2()) # Print summary
+
+"""
+Final linear regression model showing use of which drug categories has most
+substantial effect on wellbeing.
+"""
+# Remove insignificant variables from model
+X = [df1.devrstm, df1.devrpsy, df1.devropi, df1.devrcla, df1.devrps]
+X = np.array(X)
+X = X.T
+X = sm.add_constant(X) # Include constant
+
+# Run second linear regression model without insignificant variables
+linear_model3=sm.OLS(y,X)
+result_lin3=linear_model3.fit()
+print(result_lin3.summary2()) # Print summary
